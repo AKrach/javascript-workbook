dde2915d2b5006aee3a619375fe8b5751f9e4c40
Adrian Krach <mbairak@Adrians-MacBook-Air.local>
2017-08-03 14:27:34 -0500

    tictactoe changes

--- 02week/ticTacToe.js
+++ 02week/ticTacToe.js
@@ -25,22 +25,66 @@ function printBoard() {
 
 function horizontalWin() {
   // Your code here
+  if (board[0][0] && board[0][1] && board[0][2] === playerTurn ||
+  board[1][0] && board[1][1] && board[1][2] === playerTurn ||
+  board[2][0] && board[2][1] && board[2][2] === playerTurn) {
+    return true;
+  }
+
 }
 
 function verticalWin() {
   // Your code here
+  if (board[0][0] && board[1][0] && board[2][0] === playerTurn ||
+  board[0][1] && board[1][1] && board[2][1] === playerTurn ||
+  board[0][2] && board[1][2] && board[2][2] === playerTurn) {
+    return true;
+  }
 }
 
 function diagonalWin() {
   // Your code here
+  if (board[0][0] && board[1][1] && board[2][2] === playerTurn ||
+  board[0][2] && board[1][1] && board[2][0] === playerTurn) {
+    return true;
+  }
 }
 
 function checkForWin() {
   // Your code here
+  if (horizontalWin() || verticalWin() || diagonalWin()) {
+    return true;
+  }
 }
 
 function ticTacToe(row, column) {
-  // Your code here
+  if(!board[row][column].trim()){
+      const updatedBoard = [...board, 5];
+      // can't w/o an immutable lib
+      // slice the row, store to const, (not splice bc that doesn't return the row)
+      const updatedRow = updatedBoard.slice(row, row+1)[0];
+      // console.log(updatedRow, 'ROW');
+      // at the col index of that row splice the marker
+      updatedRow.splice(column, 1, playerTurn);
+      // console.log(updatedRow, 'ROW');
+      // splice new row to copy of board
+      // set old borad to new board
+      updatedBoard.splice(row, 1, updatedRow);
+
+      // console.log(board, 'board');
+    /*Check for the wins*/
+    if(checkForWin()){
+        board = [[' ', ' ', ' '],[' ', ' ', ' '],[' ', ' ', ' ']];
+        console.log(`Player ${playerTurn} wins`);
+    }else{
+        playerTurn = playerTurn === 'X' ? 'O' : 'X';
+        return board;
+    }
+    /*If any wins come back return the player winner, clear the board*/
+    /*If no wins, switch the player, display the updated board*/
+  }else{
+    console.log('Space taken, try again');
+  }
 }
 
 function getPrompt() {
