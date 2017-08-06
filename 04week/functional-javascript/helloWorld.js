6f3938a12be286a75b347a383f114a14ab3a887b
Adrian Krach <mbairak@Adrians-MacBook-Air.local>
2017-08-05 21:38:23 -0500

    white board

--- 04week/functional-javascript/helloWorld.js
+++ 04week/functional-javascript/helloWorld.js
@@ -1,5 +1,22 @@
-function upperCaser(input) {
-  return input.toUpperCase();
-}
+// function upperCaser(input) {
+//   return input.toUpperCase();
+// }
+//
+// module.exports = upperCaser
 
-module.exports = upperCaser
+// function repeat(operation, num) {
+//   for (var i = 0; i < num; i++) {
+//     operation();
+//   }
+// }
+//
+// // Do not remove the line below
+// module.exports = repeat
+
+function doubleAll(numbers) {
+      // SOLUTION GOES HERE
+  const double = numbers.map((numbers) => {
+    return numbers * 2;
+  });
+}
+module.exports = doubleAll
