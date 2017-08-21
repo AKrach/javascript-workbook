3fa3a9289b78f4cbddbb71ff7c4acd15505db37f
Adrian Krach <mbairak@Adrians-MacBook-Air.local>
2017-08-21 15:13:15 -0500

    added win states

--- 06week/ticTacToe/script.js
+++ 06week/ticTacToe/script.js
@@ -2,4 +2,27 @@
 
 document.addEventListener('DOMContentLoaded', () => {
   // Your code here
+  const winState = [
+    // horizontal
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    // vertical
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    // diagonal
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+  
+
+
+
+
+
+
+
+
+
 });
